873bbe0420c905afa841d99e754c48562e4d14de
Farhan Khan <farhannaveed6@gmail.com>
2019-01-29 10:18:43 -0500

    intergrated timestamp and database

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -48,5 +48,15 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create Snippet"))
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
