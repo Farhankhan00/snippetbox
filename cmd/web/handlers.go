3a1c9d4679f8de3956825aeeb0f37f23fa91d3de
Farhankhan00 <42901233+Farhankhan00@users.noreply.github.com>
2019-02-27 23:02:47 -0500

    Dev (#4)
    * Ch02 (#3)
    
    * get test running
    
    * update via go mod tidy
    
    * implemeniting catche runtime
    
    * implementing middleware

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 
@@ -23,25 +22,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
-	// files := []string{
-	// 	"./ui/html/home.page.tmpl",
-	// 	"./ui/html/base.layout.tmpl",
-	// 	"./ui/html/footer.partial.tmpl",
-	// }
 
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// 	return
-	// }
-	// err = ts.Execute(w, nil)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// 	return
-	// }
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -58,23 +42,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	} else if err != nil {
 		app.serverError(w, err)
 	}
-
-	data := &templateData{Snippet: s}
-
-	files := []string{
-		"./ui/html/show.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-	err = ts.Execute(w, data)
-	if err != nil {
-		app.serverError(w, err)
-	}
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Farhankhan00/snippetbox
 
-require github.com/go-sql-driver/mysql v1.4.1
+require (
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/justinas/alice v0.0.0-20171023064455-03f45bd4b7da
+	google.golang.org/appengine v1.4.0 // indirect
+)
