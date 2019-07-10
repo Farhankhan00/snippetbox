fd153139be0a36dcb2e38ba78942ae5d472d1ce4
Farhan Khan <farhannaveed6@gmail.com>
2019-07-09 23:59:30 -0400

    passwod

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Farhankhan00/snippetbox/pkg/models"
 	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // UserModel models
@@ -15,12 +16,12 @@ type UserModel struct {
 
 // Insert insert
 func (m *UserModel) Insert(name, email, password string) error {
-	hashedPassword, err := bcrypt.GeneratePassword([]byte(password), 12)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	if err != nil {
 		return nil
 	}
 	stmt := `INSERT INTO users (name, email, hashed_password, created) VALUES(?, ?, ?, UTC_TIMESTAMP())`
-	__, err = m.DB.Exec(stmt, name, email, string(hasehedPassword))
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
 	if err != nil {
 		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
 			if mysqlErr.Number == 1062 && strings.Contains(mysqlErr.Message, "users_uc_email") {
